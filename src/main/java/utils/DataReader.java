a7eed7e85043466a5430987968aa1698189019a1
sanju <sanju.scaria@gmail.com>
2025-09-16 15:04:51 +0530

    constructor added to DataReader.java

--- src/main/java/utils/DataReader.java
+++ src/main/java/utils/DataReader.java
@@ -2,4 +2,14 @@ package utils;
 
 public class DataReader {
 //added from github for testing push 
+String filePath;
+
+public DataReader(String filePath) {
+	super();
+	this.filePath = filePath;
+}
+
+	
+	
+	
 }
